675c937e72109528f1093c2461dc898759e9fbc6
5H1N0 <shinosamuel94@gmail.com>
2021-10-23 18:07:14 +0400

    added a save model function

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-PROJECT_NAME = "ANN-implementation-DLCVNLP"
-USER_NAME = "5H1N0"
-
-setuptools.setup(
-    name= f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
-    author=USER_NAME,
-    author_email="shinosamuel94@gmail.com",
-    description="Its a implementation of ANN",
+setup(
+    name="src",
+    version="0.0.2",
+    author="5H1N0",
+    description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url= f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    url="https://github.com/5H1N0/ANN-implementation-DLCVNLP",
+    author_email="shinosamuel94@gmail.com",
+    packages=["src"],
     python_requires=">=3.7",
-    install_requires = [
+    install_requires=[
         "tensorflow",
         "matplotlib",
         "seaborn",
         "numpy",
         "pandas"
-      
     ]
 )
